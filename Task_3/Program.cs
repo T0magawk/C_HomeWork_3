e40b8161295cf1006359547ea27b73d7ca72c92b
Александр Шушпанов <aleksandrshushpanov@MacBook-Air-Aleksandr.local>
2023-02-18 12:29:34 +0300

    Задача_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//* при N < 0, нужно вывести от N до -1
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Ведите число N:");
+int n = int.Parse(Console.ReadLine()!);
+int index = 1;
+int a = 0;
+Console.WriteLine($"кубы числе от 1 до {n}:");
+while (index <= n)
+{
+    a = index * index * index;
+    Console.WriteLine(a);
+    index = index + 1;
+}
+
